13bac9a9b693f64a354feff9ae172df6bf5194b9
talia hsia <thsia30@stuy.edu>
2022-09-22 23:42:11 -0400

    warmup2 test cases

--- 03-py/warmup2/warmup2.py
+++ 03-py/warmup2/warmup2.py
@@ -1,16 +1,29 @@
 # Talia Hsia - Coding Bat - Warmup2
 
 #string times
-def front3(str):
-  if len(str) > 2:
-    return str[0:3] *3
-  return str *3
+def string_times(str, n):
+  return str *n
+
+print(string_times('Hi', 2))
+print("ans: HiHi")
+print(string_times('Hi', 3))
+print("ans: HiHiHi")
+print(string_times('Hi', 1))
+print("ans: Hi")
+
 
 #front times
 def front_times(str, n):
   if len(str) > 2:
     return str[0:3] * n
   return str *n
+
+print(front_times('Chocolate', 2))
+print("ans: ChoCho")
+print(front_times('Chocolate', 3))
+print("ans: ChoChoCho")
+print(front_times('Abc', 3))
+print("ans: AbcAbcAbc")
 
 #string bits
 def string_bits(str):
@@ -20,12 +33,26 @@ def string_bits(str):
       final += str[i:i+1]
   return final
 
+print(string_bits('Hello'))
+print("ans: Hlo")
+print(string_bits('Hi'))
+print("ans: H")
+print(string_bits('Heeololeo'))
+print("ans: Hello")
+
 #string splosion
 def string_splosion(str):
   final = ""
   for i in range(len(str)):
     final += str[0:i+1]
   return final
+
+print(string_splosion('Code'))
+print("ans: CCoCodCode")
+print(string_splosion('abc'))
+print("ans: aababc")
+print(string_splosion('ab'))
+print("ans: aab")
 
 #last2
 def last2(str):
@@ -37,6 +64,14 @@ def last2(str):
       count += 1
   return count
 
+
+print(last2('hixxhi'))
+print("ans: 1")
+print(last2('xaxxaxaxx'))
+print("ans: 1")
+print(last2('axxxaaxx'))
+print("ans: 2")
+
 #array count9
 def array_count9(nums):
   count = 0
@@ -44,6 +79,13 @@ def array_count9(nums):
     if (nums[i] == 9):
       count += 1
   return count
+
+print(array_count9([1, 2, 9]))
+print("ans: 1")
+print(array_count9([1, 9, 9]))
+print("ans: 2")
+print(array_count9([1, 9, 9, 3, 9]))
+print("ans: 3")
 
 #array front9
 def array_front9(nums):
@@ -59,6 +101,13 @@ def array_front9(nums):
         return True
   return False
 
+print(array_front9([1, 2, 9, 3, 4]))
+print("ans: True")
+print(array_front9([1, 2, 3, 4, 9]))
+print("ans: False")
+print(array_front9([1, 2, 3, 4, 5]))
+print("ans: False")
+
 #array123
 def array123(nums):
   i = 0
@@ -67,6 +116,13 @@ def array123(nums):
       if(nums[i+1] == 2 and nums[i+2] == 3):
         return True
   return False
+
+print(array123([1, 1, 2, 3, 1]))
+print("ans: True")
+print(array123([1, 1, 2, 4, 1]))
+print("ans: False")
+print(array123([1, 1, 2, 1, 2, 3]))
+print("ans: True")
 
 #string match
 def string_match(a, b):
@@ -81,4 +137,9 @@ def string_match(a, b):
       count += 1
   return count
 
-
+print(string_match('xxcaazz', 'xxbaaz'))
+print("ans: 3")
+print(string_match('abc', 'abc'))
+print("ans: 2")
+print(string_match('abc', 'axc'))
+print("ans: 0")
