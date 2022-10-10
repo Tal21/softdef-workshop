8ff1cca3bd8c17ad15bff86fe158bc64a6c69925
talia hsia <thsia30@stuy.edu>
2022-10-09 23:55:18 -0400

    transferring code 08_serve

---
+++ 08_serve/v0/app.py
@@ -0,0 +1,13 @@
+# Clyde 'Thluffy' Sinclair
+# SoftDev
+# Oct 2022
+
+from flask import Flask
+app = Flask(__name__) # it creates an instance of class Flask within a directory
+
+@app.route("/") # assign fxn to route
+def hello_world():
+    print(__name__) # prints name of class in terminal
+    return "No hablo queso!"  # prints string on website
+
+app.run()  # runs site
